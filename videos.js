535d6138030354784b60d1c6e1aa3ea95c840ab0
Ayappii-VRC <ayappiivrc@gmail.com>
2025-11-05 13:19:54 +0900

    Update videos.js

--- videos.js
+++ videos.js
@@ -111,5 +111,13 @@ window.VIDEOS = [
     caption: { ja: "手話に興味あるかた、いらっしゃい！", en: "come join us!" },
     src: "https://youtu.be/0Wfs1r2PGHs?si=SOkwqQMfEtpw6b8u"
   },
+  {
+    id: "v15",
+    title: { ja: "【限定公開・共有禁止】「全国手話検定級4級」手話検定対策アーカイブ", en: "【DO NOT SHARE】Event Archive" },
+    datePosted: "2025-11-3",
+    tags: ["Archive"],
+    caption: { ja: "【担当】そりん【通訳】ユウ", en: "Host: Seorin | Translator: Yu" },
+    src: "https://youtu.be/7PP8zfnXfFY"
+  },
   
 ];
